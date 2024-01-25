b25d8e957fb430a79a4e426407644bbbfd39f065
Charles Pereira <charlespereira.fln@gmail.com>
2024-01-24 21:06:44 -0300

    Desafio 1

---
+++ desafios/desafio1.js
@@ -0,0 +1,10 @@
+
+alert("Boas vindas ao nosso site!")
+
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+
+if (idade >= 18) {
+    alert("Pode tirar a habilitação")
+} else 
+    alert("Você é menor de idade, não pode tirar a habilitação!")
