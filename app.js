4b95485e551a5de1c9f36d70c10ab55e1f7208a4
Charles Pereira <charlespereira.fln@gmail.com>
2024-01-25 01:09:08 -0300

    Concatenar

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao Jogo do número secreto.');
-let numeroSecreto = 29;
+let numeroSecreto = 4;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
+//Abra o console para ver a mensagem do console.
 if (numeroSecreto == chute) {
-    console.log('Isso aí! Você descobriu o número secreto (29)');
+    alert(`Isso aí! Você descobriu o número secreto  ${numeroSecreto}`); // Template Strings: usando CRASE ${ variavel }
+} else {
+    alert('Você errouuuu')
 }
