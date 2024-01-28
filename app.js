32c8827a915385d819555ee92e3461e6ed4f27b8
Charles Pereira <charlespereira.fln@gmail.com>
2024-01-28 03:53:26 -0300

    Desafio 3 + while

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 alert('Boas vindas ao Jogo do número secreto.');
-let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
-console.log('Valor do chute:', chute);
-console.log('Resultado da comparação:', chute == numeroSecreto);
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-//Abra o console para ver a mensagem do console.
-if (numeroSecreto == chute) {
-    alert(`Isso aí! Você descobriu o número secreto  ${numeroSecreto}`); // Template Strings: usando CRASE ${ variavel }
-} else {
-    alert('Você errouuuu')
+// Enquanto o chute não for igual ao número secreto.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto  ${numeroSecreto} com ${tentativas} tentativas.`); // Template Strings: usando CRASE ${ variavel }
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
